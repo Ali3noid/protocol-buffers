55a2e239eff575088bd14b3174afcf1a03e51dc8
Rafal Nowakowski <rafal1nowakowski@gmail.com>
2018-07-02 23:33:16 +0200

    Initial commit

---
+++ src/main/java/com/turbo/protocolbuffers/ProtocolBuffersApplication.java
@@ -0,0 +1,12 @@
+package com.turbo.protocolbuffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocolBuffersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtocolBuffersApplication.class, args);
+    }
+}
